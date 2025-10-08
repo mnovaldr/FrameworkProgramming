f49ad3032cd0d9f3cd37d7e5d8d25b4b7f2f6118
mnovaldr <mnovaldr@gmail.com>
2025-10-09 06:56:11 +0700

    Update Challlenge

--- DataKelurahan/warga/views.py
+++ DataKelurahan/warga/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView  
 from .models import Warga
 
 class WargaListView(ListView):
     model = Warga
     template_name = 'warga/warga_list.html'
+
+class WargaDetailView(DetailView):
+    model = Warga
+    template_name = 'warga/warga_detail.html'
