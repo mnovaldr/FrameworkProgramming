3500ed39e6c786bf95b3cecbc359879359cd84e5
mnovaldr <mnovaldr@gmail.com>
2025-10-23 06:34:14 +0700

    Challenge 4: Updateview dan Deleteview CRUD lengkap

--- DataKelurahan/warga/urls.py
+++ DataKelurahan/warga/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 from .views import (
     WargaListView, WargaDetailView, PengaduanListView,
-    WargaCreateView, PengaduanCreateView
+    WargaCreateView, PengaduanCreateView, WargaUpdateView,
+    WargaDeleteView, PengaduanUpdateView, PengaduanDeleteView
 )
 
 urlpatterns = [
     path('', WargaListView.as_view(), name='warga-list'),
     path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
-    path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
     path('tambah/', WargaCreateView.as_view(), name='warga-tambah'),
+    path('<int:pk>/edit/', WargaUpdateView.as_view(), name='warga-edit'),
+    path('<int:pk>/hapus/', WargaDeleteView.as_view(), name='warga-hapus'),
+
+    path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
     path('pengaduan/tambah/', PengaduanCreateView.as_view(), name='pengaduan-tambah'),
+    path('pengaduan/<int:pk>/edit/', PengaduanUpdateView.as_view(), name='pengaduan-edit'),
+    path('pengaduan/<int:pk>/hapus/', PengaduanDeleteView.as_view(), name='pengaduan-hapus'),
+    
 ]
--- DataKelurahan/warga/views.py
+++ DataKelurahan/warga/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView  
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from .models import Warga, Pengaduan
 from .forms import WargaForm, PengaduanForm
 
@@ -29,4 +29,29 @@ class PengaduanCreateView(CreateView):
     model = Pengaduan
     form_class = PengaduanForm
     template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list')
+
+class WargaUpdateView(UpdateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga-list')
+
+
+class WargaDeleteView(DeleteView):
+    model = Warga
+    template_name = 'warga/warga_confirm_delete.html'
+    success_url = reverse_lazy('warga-list')
+
+
+class PengaduanUpdateView(UpdateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list')
+
+
+class PengaduanDeleteView(DeleteView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_confirm_delete.html'
     success_url = reverse_lazy('pengaduan-list')
